c33cec037f124c915ea360fbaa49ca3fdc6e2e50
LoveYourMommy <rbhbynjh1021>
2013-03-27 13:08:10 +0300

    Made an exercise

---
+++ exercise_class_turtle.py
@@ -0,0 +1,29 @@
+__author__ = 'lym'
+
+import turtle
+
+a = turtle.Pen()
+b = turtle.Pen()
+c = turtle.Pen()
+d = turtle.Pen()
+
+a.forward(100)
+b.forward(100)
+c.forward(120)
+d.forward(120)
+a.left(90)
+b.right(90)
+a.forward(50)
+b.forward(50)
+a.right(90)
+b.left(90)
+a.forward(70)
+b.forward(70)
+c.left(90)
+d.right(90)
+c.forward(30)
+d.forward(30)
+c.right(90)
+d.left(90)
+c.forward(40)
+d.forward(40)
